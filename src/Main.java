83e2964d31534ef772c91010bcd8c46e5da746c6
Alinaprish <114585491+Alinaprish@users.noreply.github.com>
2022-11-18 18:44:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import jdk.internal.org.objectweb.asm.tree.LocalVariableAnnotationNode;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 55;
-        double b = (int) 1.77;
+        int weight = 55;
+        double growth = (int) 1.77;
         BmiService service = new BmiService();
         int MyIndex  = service.calculate(55, 1.77);
         System.out.println(MyIndex);
